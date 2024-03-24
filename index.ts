5e94f97b09021f8d875419a32ac943bcfe4d81c8
Nida Naz <162494835+NidaNaz804@users.noreply.github.com>
2024-03-24 17:29:37 +0500

    This is a simple calculator

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform opration",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "subtraction", "Multiplication", "Division"],
+  },
+]);
+
+//conditional statement
+if (answer.operator === "Multiplication"){
+    console.log("your value is " + answer.firstNumber * answer.secondNumber)
+}
+else if (answer.operator === "Addition")
+console.log("your value is " + answer.firstNumber + answer.secondNumber);
+else if (answer.operator === "subtraction")
+console.log("your value is " , answer.firstNumber - answer.secondNumber);
+else if (answer.operator === "Division")
+console.log("your value is " + answer.firstNumber / answer.secondNumber);
+else { console.log ("please Select your valid opertor");
+}
